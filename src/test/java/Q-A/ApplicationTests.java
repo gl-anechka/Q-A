b9d181e8177ec215a14cffc7d5a7e814bb4fcdfa
gl-anechka <gl_anechka@mail.ru>
2026-05-13 00:11:35 +0300

    init commit

---
+++ src/test/java/Q-A/ApplicationTests.java
@@ -0,0 +1,13 @@
+package QandA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
